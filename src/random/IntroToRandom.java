938dc0c5f435174b17f157c38667f9c2afa67f3d
ANYA1128 <League@AdministorsiMac.attlocal.net>
2018-04-11 19:57:06 -0700

    IntroToRandom

--- src/random/IntroToRandom.java
+++ src/random/IntroToRandom.java
@@ -12,7 +12,10 @@ public class IntroToRandom {
 		
 		//3. Now make r random. 
 		//   Create an object of the Random class
+	       Random gen = new Random();
+	       
 		//	 use .nextInt() to get a random number
+		   r=gen.nextInt(100);
 		
 
 		//4. Limit the random number between 0 and 100
@@ -22,7 +25,7 @@ public class IntroToRandom {
 		//6. Challenge: Limit the random number between -222 and 88
 
 		//1. Print out the value of r
-		
+		    System.out.println(r);
 		//2. Run the program.  What number appears in the console?
 		//   Run it again. Is the number the same?
 	}
